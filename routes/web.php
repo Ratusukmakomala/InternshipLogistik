dbae30a9ae7e6a7b3756575b052765f783894c1e
Ratusukmakomala <ratukomala28@gmail.com>
2025-02-16 15:34:25 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('auth/login');
+    return redirect()->route('auth.login');
 });
 
 Route::get('/dashboard', App\Http\Controllers\Admin\DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
